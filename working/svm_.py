5971b3b736a4731d969aae5f68a457548425eabc
jakma <3562412848@qq.com>
2025-12-02 15:49:10 +0800

    feat:Emotion becomes good.

--- working/svm_.py
+++ working/svm_.py
@@ -8,6 +8,7 @@ import numpy as np
 import time
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
+from feature import apply_pca
 
 def evaluate_svm(all_data, label):
     y = np.array([list(label+1)*3 for _ in range(15)])
@@ -17,8 +18,8 @@ def evaluate_svm(all_data, label):
     for train_idx, test_idx in tqdm(loo.split(all_data), desc='[SVM Training]'):
         X_train, X_test = all_data[train_idx], all_data[test_idx]   # [14, 3, 15, 310], [1, 3, 15, 310]
         y_train, y_test = y[train_idx], y[test_idx]     # [14, 45], [1, 45]
-        X_train = X_train.reshape(-1, 310)
-        X_test = X_test.reshape(-1, 310)
+        X_train = X_train.reshape(-1, 100)
+        X_test = X_test.reshape(-1, 100)
         y_train = y_train.flatten()
         y_test = y_test.flatten()
 
@@ -48,41 +49,6 @@ def evaluate_svm(all_data, label):
     
     return acc_results
 
-def evaluate_lightgbm(all_data, label):
-    y = np.array([list(label+1)*3 for _ in range(15)])
-    params = {'objective': 'multiclass',
-        'num_class': 3,
-        'learning_rate': 0.05,
-        'num_leaves': 63,
-        'metric': 'multi_logloss'}
-    loo = LeaveOneOut()
-    acc_results = []
-    
-    for train_idx, test_idx in tqdm(loo.split(all_data), desc='[LightGBM Training]'):
-        X_train, X_test = all_data[train_idx], all_data[test_idx]
-        y_train, y_test = y[train_idx], y[test_idx]    
-        X_train = X_train.reshape(-1, 310)
-        X_test = X_test.reshape(-1, 310)
-        y_train = y_train.flatten()
-        y_test = y_test.flatten()
-        train_dataset = lgb.Dataset(X_train, label=y_train)
-        test_dataset = lgb.Dataset(X_test, label=y_test)
-
-        model = lgb.train(params, train_dataset, valid_sets=[test_dataset], num_boost_round=80)
-        pred = model.predict(X_test)    # 概率值
-        pred = np.argmax(pred, axis=1)
-        acc = accuracy_score(y_test, pred)
-        acc_results.append(acc)
-
-    return acc_results
-
-def evaluate_model(all_data, label, model_name):
-    print(f'Choose evaluated model:[{model_name}]')
-    if model_name == "svm":
-        return evaluate_svm(all_data, label)
-    elif model_name == 'lightgbm':
-        return evaluate_lightgbm(all_data, label)
-
 def print_results(acc_results):
     print(f'SVM model evaluate mean score: {np.mean(acc_results):.3f}')
 
@@ -90,12 +56,13 @@ def main():
     print('Run Start!')
     print('Load data...')
     all_data = load_data()
+    print('Apply PCA...')
+    all_data = apply_pca(all_data, n_components=100)
     print('Load label...')
     label = sio.loadmat('input/label.mat')['label'].squeeze(0)
     print('Train model...')
-    # model_name = 'lightgbm'
     model_name = 'svm'
-    acc_results = evaluate_model(all_data, label, model_name)
+    acc_results = evaluate_svm(all_data, label)
     print('\nVisualize Test Results...')
     visualize_subjects(acc_results, model_name)
     print_results(acc_results)
